bfaa3087b8df2f19397d99f513ebd8d07c5d9872
unknown <mclaravisconti@gmail.com>
2025-07-02 10:44:51 -0300

    eliminar archivos antiguos

--- Usuarios.py
+++
@@ -1,7 +0,0 @@
-usuarios = [
-    {"nombre": "Clara", "contraseña": 1234},
-    {"nombre": "María", "contraseña": 5678},
-    {"nombre": "Carlos", "contraseña": 9101},
-    {"nombre": "Lucía", "contraseña": 1121},
-    {"nombre": "Martín", "contraseña": 3141},
-]
